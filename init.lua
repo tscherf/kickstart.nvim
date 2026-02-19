e8b33c9bd6d15f5b1e45d913b57f4c35eb995197
Thorsten Scherf <tscherf@redhat.com>
2026-02-19 13:10:05 +0100

    Add gruvbox color scheme

--- init.lua
+++ init.lua
@@ -794,20 +794,23 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    --'folke/tokyonight.nvim',
+    'gruvbox-community/gruvbox',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
+    init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'gruvbox'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+
+      -- Highlight misspelled words with a light red background instead of underline
+      vim.api.nvim_set_hl(0, 'SpellBad', { bg = '#f28d8d', undercurl = false, underline = false })
+      vim.api.nvim_set_hl(0, 'SpellCap', { bg = '#f28d8d', undercurl = false, underline = false })
+      vim.api.nvim_set_hl(0, 'SpellRare', { bg = '#f28d8d', undercurl = false, underline = false })
+      vim.api.nvim_set_hl(0, 'SpellLocal', { bg = '#f28d8d', undercurl = false, underline = false })
     end,
   },
 
