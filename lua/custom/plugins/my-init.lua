cffd851c967d65fe6d831b3f1384015c2d63d046
Thorsten Scherf <tscherf@redhat.com>
2026-06-18 17:49:13 +0200

    Add floating help plugin

--- lua/custom/plugins/my-init.lua
+++ lua/custom/plugins/my-init.lua
@@ -5,3 +5,14 @@ require('gruvbox').setup {}
 
 vim.cmd.colorscheme 'gruvbox'
 
+-- Floating help window
+vim.pack.add { 'https://github.com/Tyler-Barham/floating-help.nvim' }
+require('floating-help').setup ({
+      width = 80,
+      height = 0.9,
+      position = 'E',
+      border = 'rounded',
+    })
+vim.keymap.set('n', '<leader>h', require('floating-help').toggle, { desc = 'Floating [h]help' })
+
+
