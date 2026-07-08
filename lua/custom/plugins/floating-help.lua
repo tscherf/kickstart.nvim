c8637919b24b7a172cd3789753e135330ebbf995
Thorsten Scherf <tscherf@redhat.com>
2026-07-08 15:18:18 +0200

    Restructure custom lua files

---
+++ lua/custom/plugins/floating-help.lua
@@ -0,0 +1,11 @@
+-- Floating help window
+vim.pack.add { 'https://github.com/Tyler-Barham/floating-help.nvim' }
+require('floating-help').setup ({
+      width = 80,
+      height = 0.9,
+      position = 'E',
+      border = 'rounded',
+    })
+vim.keymap.set('n', '<leader>h', require('floating-help').toggle, { desc = 'Floating [h]help' })
+
+
