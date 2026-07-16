3e46dbcef68b1ad1eabc49cbbb760e69fdbbc928
Thorsten Scherf <tscherf@redhat.com>
2026-07-16 10:34:22 +0200

    Add spellcheck and highlighting

--- lua/custom/plugins/my-init.lua
+++ lua/custom/plugins/my-init.lua
@@ -1,3 +1,7 @@
 -- Join current paragraph until next blank line
 vim.keymap.set('n', '<leader>J', 'vipJ', { desc = 'Join paragraph' })
 
+-- Perform spell check in German and English
+vim.keymap.set('n', '<leader>spg', '<cmd>set spell spelllang=de<CR>', { desc = 'Spellcheck German' })
+vim.keymap.set('n', '<leader>spe', '<cmd>set spell spelllang=en<CR>', { desc = 'Spellcheck English' })
+vim.cmd('highlight! SpellBad guisp=Red gui=underline')
